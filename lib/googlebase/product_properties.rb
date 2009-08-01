24dd6297724d45bf501078355250f1e6959ef88c
Carl Porth <badcarl@gmail.com>
2009-08-01 14:42:20 -0700

    Cleaned up ProductProperties

--- lib/googlebase/product_properties.rb
+++ lib/googlebase/product_properties.rb
@@ -4,28 +4,31 @@ require 'dm-validations'
 
 module GoogleBase
   ProductProperties = LazyModule.new do
-    property :id, String, :key => true, :nullable => true, :length => 255, :from_xml => 'xmlns:id'
-    property :title, String, :from_xml => 'xmlns:title', :length => 70
-    property :description, DataMapper::Types::Text, :field => 'content', :from_xml => 'xmlns:content', :lazy => false
-    property :link, URI, :from_xml => "xmlns:link[@rel='alternate']/@href",
+    Text = DataMapper::Types::Text
+
+    property :id,           String, :key => true, :nullable => true, :length => 255, :from_xml => 'xmlns:id'
+    property :title,        String, :from_xml => 'xmlns:title', :length => 70
+    property :description,  Text,   :field => 'content', :from_xml => 'xmlns:content', :lazy => false
+    property :link,         URI,
+      :from_xml => "xmlns:link[@rel='alternate']/@href",
       :to_xml => lambda { |xml, value| xml.link :href => value, :type => 'text/html', :rel => 'alternate' }
-    property :condition, String, :field => 'g:condition', :nullable => false # :set => %w[ new used refurbished ], :default => 'new',
+    property :condition,    String, :field => 'g:condition',    :nullable => false # :set => %w[ new used refurbished ], :default => 'new',
     property :product_type, String, :field => 'g:product_type', :length => 255
-    property :image_link, URI, :field => 'g:image_link'
-    property :product_id, String, :field => 'g:id', :nullable => false
-    property :price, String, :field => 'g:price', :nullable => false
-    property :brand, String, :field => 'g:brand'
-    property :item_type, String, :field => 'g:item_type', :nullable => false, :set => %w[ Products Produkte ], :default => 'Products'
+    property :image_link,   URI,    :field => 'g:image_link'
+    property :product_id,   String, :field => 'g:id',           :nullable => false
+    property :price,        String, :field => 'g:price',        :nullable => false
+    property :brand,        String, :field => 'g:brand'
+    property :item_type,    String, :field => 'g:item_type',    :nullable => false, :set => %w[ Products Produkte ], :default => 'Products'
 
     # optional
-    property :expires_at, DateTime, :field => 'g:expiration_date',
+    property :expires_at,       DateTime, :field => 'g:expiration_date',
       :to_xml => lambda { |xml, value| xml.send 'g:expiration_date', value.strftime('%F') }
-    property :quantity, Integer, :field => 'g:quantity'
+    property :quantity,         Integer, :field => 'g:quantity'
     property :payment_accepted, String,
       :from_xml => lambda { |entry| entry.xpath('./g:payment').map { |e| e.content }.join(',') },
       :to_xml => lambda { |xml, values| values.split(',').each { |value| xml.send 'g:payment_accepted', value } }
-    property :item_language, String,  :field => 'g:item_language'
-    property :target_country, String, :field => 'g:target_country'
+    property :item_language,    String,  :field => 'g:item_language'
+    property :target_country,   String, :field => 'g:target_country'
 
     # read only
     property :created_at,   DateTime, :field => 'xmlns:published',          :to_xml => false
